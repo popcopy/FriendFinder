469e4c14ab2d6bdef6c86907cc562f8a81fc968c
Matt Urian <matt.a.urian@gmail.com>
2019-02-17 17:46:13 -0500

    heroku

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ require('./app/routing/apiRoutes.js')(app, path);
 require('./app/routing/htmlRoutes.js')(app, path);
 
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
